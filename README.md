114262941ac645761caff27a8ae09220d57b250c
Creat0s <866creat0s@gmail.com>
2023-09-12 01:42:43 +0400

    test

---
+++ README.md
@@ -0,0 +1,32 @@
+# npuzzle
+
+Npuzzle is a small library that allows you to create *NxM puzzles* that operate on the
+principles of *15 puzzles*. This repository also contains an implementation and a copy
+of the game using the [raylib](https://github.com/raysan5/raylib) library.
+
+## Game
+
+The game uses only *NxN puzzles*, where *3 <= N <= 7*. However, this does not prevent
+you from creating puzzles of different dimensions in your projects using this library.
+The game was built for Microsoft Windows and Android platforms. To manually build the
+provided game example, you will need to use the MAKEFILE from the [raylib](https://github.com/raysan5/raylib) library.
+
+## Lib
+
+The library consists of 3 functions. The function *create_puzzle(array, sizeN, sizeM)*
+takes a pointer to a one-dimensional array of integers *int*, where *sizeN* is the
+number of horizontal columns, and *sizeM* is the number of vertical columns.
+*create_puzzle()* starts filling your array with numbers and then launches the *is_solvable()*
+function. If the generated puzzle is solvable, *create_puzzle()* stops its work; otherwise,
+it continues generating puzzles. The *count_inversions()* function is called by *is_solvable()*
+to obtain the number of inversions in the puzzle. These functions work the same way on any platform.
+
+## Supported platforms (Game)
+
+| Name              |   Version   |
+| ----------------- | ----------- |
+| Windows           | 7/8.1/10/11 |
+| Android           | 7.0+        |
+
+You can easily rebuild this sample game on other platforms supported by [raylib](https://github.com/raysan5/raylib).
+You also can download the game from the following link: 
--- main.c
+++ main.c
@@ -2,39 +2,49 @@
 #include "npuzzle.h"
 #include <stdio.h>
 
+//#include "devel.h"
+
 #define SCREEN_WIDTH 360
 #define SCREEN_HEIGHT 640
 
-#define RADIUS 5
+#define TOUCH_RADIUS 5
 #define MAX_DIFFICULTY 5
 
 #if defined (__ANDROID__)
-    #define GET_POSITION GetTouchPosition(0)
-    #define PRESSED (GetTouchPointCount() > 0) && (!check_position(position))
+    #define SCREEN_DOWN (GetTouchPointCount() > 0) && (!check_touch_position(current_position))
 #else
-    #define GET_POSITION GetMousePosition()
-    #define PRESSED IsMouseButtonPressed(MOUSE_LEFT_BUTTON)
+    #define SCREEN_DOWN IsMouseButtonPressed(MOUSE_LEFT_BUTTON)
 #endif
 
-int i, j;               // Counter
+int i, j;
 char buffer[3];
 bool is_mute = false;
-Vector2 position;
+Vector2 current_position;
 
 typedef struct
 {
-    Vector2 pos;
     char item[3];
-}   cell;
+    Vector2 position;
+}cell;
 
-bool check_position(Vector2 position)
+bool check_touch_position(Vector2 current_position)
 {
-    Vector2 pos = GetTouchPosition(0);
+    Vector2 touch_position = GetTouchPosition(0);
     
-    if((pos.x-RADIUS < position.x) && (pos.y-RADIUS < position.y) &&
-       (pos.x+RADIUS > position.x) && (pos.y+RADIUS > position.y)) return true;
-       
-    else return false;
+    if
+    (
+        (touch_position.x-TOUCH_RADIUS < current_position.x) &&
+        (touch_position.y-TOUCH_RADIUS < current_position.y) &&
+        (touch_position.x+TOUCH_RADIUS > current_position.x) &&
+        (touch_position.y+TOUCH_RADIUS > current_position.y)
+    )
+    {
+        return true;
+    }  
+    else
+    {
+        return false;
+    }
 }
 
 void draw_logo(Texture2D logo)
@@ -49,31 +59,33 @@ void npuzzle(int n, Texture2D texture, Sound click_sound)
     bool win = false;
     cell puzzle[n*n];
     int tmp, vals[n*n];
-    int posx, posy, defxy;
+    int pos_x, pos_y, def_xy;
     
-    defxy = ((9.0-n)/2) * 40;
-    posx = defxy; posy = defxy + 40;
+    def_xy = ((9.0-n)/2) * 40;
+    pos_x = def_xy;
+    pos_y = def_xy + 40;
     
     Sound move_sound = LoadSound("assets/move.ogg");
     Sound win_sound = LoadSound("assets/win.ogg");
     
-    // Заполняем список числами
     create_puzzle(vals, n, n);
     
-    //Заполняем игровое поле
     for(i = 1; i <= n*n; i++)
     {
         sprintf(puzzle[i-1].item, "%d", vals[i-1]);
         
-        puzzle[i-1].pos.x = posx;
-        puzzle[i-1].pos.y = posy;
+        puzzle[i-1].position.x = pos_x;
+        puzzle[i-1].position.y = pos_y;
         
         if(i%n == 0)
         {
-            posx = defxy;
-            posy += 40;
+            pos_x = def_xy;
+            pos_y += 40;
         }
-        else posx+=40;
+        else
+        {
+            pos_x += 40;
+        }
     }
     
     while(true)
@@ -82,62 +94,114 @@ void npuzzle(int n, Texture2D texture, Sound click_sound)
         {
             for(i = 1; i < n*n-1; i++)
             {
-                if(atoi(puzzle[i].item) < atoi(puzzle[i-1].item)) {win = false; break;}
-                win = true;
+                if(atoi(puzzle[i].item) < atoi(puzzle[i-1].item))
+                {
+                    win = false;
+                    break;
+                }
+                else
+                {
+                    win = true;
+                }
             }
         }
         
-        if(PRESSED)
+        if(SCREEN_DOWN)
         {
-            position = GET_POSITION;
+            current_position = GetMousePosition();
             
             // Sound is_mute & BACK to main
-            if(position.x >= 290 && position.y <= 40) is_mute = !is_mute;
-            if((position.y >= 555 && position.y <= 595) && (position.x >= 85 && position.x <= 275)) {if(!is_mute) {PlaySound(click_sound);} break;}
+            if(current_position.x >= 290 && current_position.y <= 40)
+            {
+                is_mute = !is_mute;
+            }
             
-            if(win != true)
+            // If BACK button pressed, close current game
+            if
+            (
+                (current_position.y >= 555 && current_position.y <= 595) &&
+                (current_position.x >= 85 && current_position.x <= 275)
+            )
+            {
+                if(!is_mute)
+                {
+                    PlaySound(click_sound);
+                }
+                
+                break;
+            }
+            
+            if(!win)
             {
                 for(i = 0; i < n*n; i++)
                 {
-                    if((position.x > puzzle[i].pos.x) &&
-                       (position.x < puzzle[i].pos.x+40) &&
-                       (position.y > puzzle[i].pos.y) &&
-                       (position.y < puzzle[i].pos.y+40))
-                                {j = i; break;}
-                                
-                    else j = -1;
+                    if
+                    (
+                        (current_position.x > puzzle[i].position.x) &&
+                        (current_position.x < puzzle[i].position.x+40) &&
+                        (current_position.y > puzzle[i].position.y) &&
+                        (current_position.y < puzzle[i].position.y+40)
+                    )
+                    {
+                        j = i;
+                        break;
+                    }
+                    else 
+                    {
+                        j = -1;
+                    }
                 }
                 
                 if(j != -1)
                 {
                     if(((j+1)%n != 0) && (atoi(puzzle[j+1].item) == 0))
                     {
-                        if(!is_mute) PlaySound(move_sound);
+                        if(!is_mute)
+                        {
+                            PlaySound(move_sound);
+                        }
+                        
                         tmp = atoi(puzzle[j].item);
+                        
                         sprintf(puzzle[j+1].item, "%d", tmp);
                         sprintf(puzzle[j].item, "%d", 0);
                     }
                     
                     else if((j%n != 0) && (atoi(puzzle[j-1].item) == 0))
                     {
-                        if(!is_mute) PlaySound(move_sound);
-                        tmp = atoi(puzzle[j].item);                    
+                        if(!is_mute)
+                        {
+                            PlaySound(move_sound);
+                        }
+                        
+                        tmp = atoi(puzzle[j].item);
+                        
                         sprintf(puzzle[j-1].item, "%d", tmp);
                         sprintf(puzzle[j].item, "%d", 0);
                     }
                     
                     else if(((j+n) < n*n) && (atoi(puzzle[j+n].item) == 0))
                     {
-                        if(!is_mute) PlaySound(move_sound);
+                        if(!is_mute)
+                        {
+                            PlaySound(move_sound);
+                        }
+                        
                         tmp = atoi(puzzle[j].item);
+                        
                         sprintf(puzzle[j+n].item, "%d", tmp);
                         sprintf(puzzle[j].item, "%d", 0);
                     }
                     
                     else if(((j-n) > -1) && (atoi(puzzle[j-n].item) == 0))
                     {
-                        if(!is_mute) PlaySound(move_sound);
+                        if(!is_mute)
+                        {
+                            PlaySound(move_sound);
+                        }
+                        
                         tmp = atoi(puzzle[j].item);
+                        
                         sprintf(puzzle[j-n].item, "%d", tmp);
                         sprintf(puzzle[j].item, "%d", 0);
                     }
@@ -151,14 +215,23 @@ void npuzzle(int n, Texture2D texture, Sound click_sound)
             
             for(i = 0; i < n*n; i++)
             {
-                DrawTexture(texture, puzzle[i].pos.x, puzzle[i].pos.y, WHITE);
-                if(atoi(puzzle[i].item) != 0) DrawText(puzzle[i].item, puzzle[i].pos.x+10, puzzle[i].pos.y+10, 20, BLUE);
+                DrawTexture(texture, puzzle[i].position.x, puzzle[i].position.y, WHITE);
+                if(atoi(puzzle[i].item) != 0)
+                {
+                    DrawText(puzzle[i].item, puzzle[i].position.x+10, puzzle[i].position.y+10, 20, BLUE);
+                }
             }
             
             DrawText("BACK", 150, 570, 20, DARKGRAY);
             
-            if(is_mute) DrawText("sound", 290, 10, 20, RED);
-            else DrawText("sound", 290, 10, 20, BLUE);
+            if(is_mute)
+            {
+                DrawText("sound", 290, 10, 20, RED);
+            }
+            else
+            {
+                DrawText("sound", 290, 10, 20, BLUE);
+            }
             
             if(win == true)
             {
@@ -178,9 +251,10 @@ void npuzzle(int n, Texture2D texture, Sound click_sound)
 int main(void)
 {
     int difficulty = 3;
-    Color diffBClr[MAX_DIFFICULTY];
+    Color diff_button_color[MAX_DIFFICULTY];
     
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib:npuzzle");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "npuzzle");
+    
     InitAudioDevice();
     SetTargetFPS(60);
     
@@ -189,37 +263,90 @@ int main(void)
     
     Sound click_sound = LoadSound("assets/click.ogg");
     
-    position.x = -1; position.y = -1;
+    current_position.x = -1; current_position.y = -1;
     
-    while (!WindowShouldClose())
+    while(!WindowShouldClose())
     {        
-        if(PRESSED)
+        if(SCREEN_DOWN)
         {
-            position = GET_POSITION;
+            current_position = GetMousePosition();
             
             //Change difficulty
-            if(position.y >= 430 && position.y <= 470)
+            if(current_position.y >= 430 && current_position.y <= 470)
             {
-                if(position.x >= 85 && position.x <= 115) {difficulty = 3; if(!is_mute) PlaySound(click_sound);}
-                else if(position.x >= 125 && position.x <= 155) {difficulty = 4; if(!is_mute) PlaySound(click_sound);}
-                else if(position.x >= 165 && position.x <= 195) {difficulty = 5; if(!is_mute) PlaySound(click_sound);}
-                else if(position.x >= 205 && position.x <= 235) {difficulty = 6; if(!is_mute) PlaySound(click_sound);}
-                else if(position.x >= 245 && position.x <= 275) {difficulty = 7; if(!is_mute) PlaySound(click_sound);}
+                if(current_position.x >= 85 && current_position.x <= 115)
+                {
+                    difficulty = 3;
+                    
+                    if(!is_mute)
+                    {
+                        PlaySound(click_sound);
+                    }
+                }
+                else if(current_position.x >= 125 && current_position.x <= 155)
+                {
+                    difficulty = 4;
+                    
+                    if(!is_mute)
+                    {
+                        PlaySound(click_sound);
+                    }
+                }
+                else if(current_position.x >= 165 && current_position.x <= 195)
+                {
+                    difficulty = 5; 
+                    
+                    if(!is_mute)
+                    {
+                        PlaySound(click_sound);
+                    }
+                }
+                else if(current_position.x >= 205 && current_position.x <= 235)
+                {
+                    difficulty = 6;
+                    if(!is_mute)
+                    {
+                        PlaySound(click_sound);
+                    }
+                }
+                else if(current_position.x >= 245 && current_position.x <= 275)
+                {
+                    difficulty = 7;
+                    if(!is_mute)
+                    {
+                        PlaySound(click_sound);
+                    }
+                }
             }
             
             //Start Game
-            else if((position.y >= 495 && position.y <= 545) && (position.x >= 85 && position.x <= 275))
+            else if
+            (
+                (current_position.y >= 495 && current_position.y <= 545) &&
+                (current_position.x >= 85 && current_position.x <= 275)
+            )
             {
-                if(!is_mute) PlaySound(click_sound);
+                if(!is_mute)
+                {
+                    PlaySound(click_sound);
+                }
+                
                 npuzzle(difficulty, cell, click_sound);
             }
             
-            //Sound is_mute
-            else if(position.x >= 290 && position.y <= 40) is_mute = !is_mute;
+            //Sound is mute
+            else if(current_position.x >= 290 && current_position.y <= 40)
+            {
+                is_mute = !is_mute;
+            }
         }
         
-        for(i = 0; i < 5; i++) diffBClr[i] = BLUE;
-        diffBClr[difficulty-3] = GREEN;
+        for(i = 0; i < 5; i++)
+        {
+            diff_button_color[i] = BLUE;
+        }
+        
+        diff_button_color[difficulty-3] = GREEN;
         
         BeginDrawing();
             
@@ -228,19 +355,26 @@ int main(void)
             draw_logo(logo);
             
             DrawText("Select difficulty", 110, 400, 18, DARKGRAY);
+            
             DrawText("Touch to start", 101, 510, 20, DARKGRAY);
             
-            if(is_mute) DrawText("sound", 290, 10, 20, RED);
-            else DrawText("sound", 290, 10, 20, BLUE);
+            if(is_mute)
+            {
+                DrawText("sound", 290, 10, 20, RED);
+            }
+            else
+            {
+                DrawText("sound", 290, 10, 20, BLUE);
+            }
             
             for(i = 0; i < MAX_DIFFICULTY; i++)
             {
                 //itoa(i+3, buffer, 10);
                 sprintf(buffer, "%d", i+3);
-                DrawText(buffer, 90+i*40, 435, 30, diffBClr[i]);
+                DrawText(buffer, 90+i*40, 435, 30, diff_button_color[i]);
             }
             
-            //drawScheme();
+            //draw_scheme(); // For the developer
             
         EndDrawing();
     }
@@ -252,5 +386,6 @@ int main(void)
     
     CloseAudioDevice();
     CloseWindow();
+    
     return 0;
 }
